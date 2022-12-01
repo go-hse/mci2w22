dd17c3193a16c06d3a1078e8190a9cc70994f3ec
Andreas Roessler <andreas.roessler@hs-esslingen.de>
2022-12-01 12:08:14 +0100

    pre fix grab

--- 11_ThreeJS_Cursor/app.mjs
+++ 11_ThreeJS_Cursor/app.mjs
@@ -3,7 +3,7 @@
 import * as THREE from '../99_Lib/three.module.js';
 
 import { mouse, keyboardInteractionFunction } from "./mouse_keyboard.mjs"
-import { createScene, boxesWithPlane, createCursor } from './scene.mjs';
+import { createScene, boxesWithPlane, createCursor, boxes2Grab, createLine } from './scene.mjs';
 
 window.onload = function () {
     console.log("ThreeJs " + THREE.REVISION);
@@ -14,15 +14,56 @@ window.onload = function () {
     world.matrixAutoUpdate = false;
     scene.add(world);
 
-    boxesWithPlane(world, 100);
+    boxesWithPlane(world, 1000);
 
     let cursor = createCursor(scene);
     mouse(cursor);
 
+    let boxes = boxes2Grab(scene, 30);
+    let lineFunc = createLine(scene);
+
+
     const updateByKeyboard = keyboardInteractionFunction();
 
+    let rayEnd = new THREE.Vector3();
+
+    let position = new THREE.Vector3();
+    let rotation = new THREE.Quaternion();
+    let scale = new THREE.Vector3();
+
+    let rayCaster = new THREE.Raycaster();
+
+    let initialGrabbed, selectedObject;
+
     function render() {
-        updateByKeyboard(world);
+        let grabbed = updateByKeyboard(world);
+        lineFunc(0, cursor.position);
+
+        let direction = new THREE.Vector3(0, 1, 0);
+        cursor.matrix.decompose(position, rotation, scale);
+        direction.applyQuaternion(rotation);
+
+        rayCaster.set(cursor.position, direction);
+        let intersects = rayCaster.intersectObjects(boxes);
+        if (intersects.length > 0) {
+            lineFunc(1, intersects[0].point);
+            selectedObject = intersects[0].object;
+            if (grabbed) {
+                if (initialGrabbed === undefined) {
+                    initialGrabbed = cursor.matrix.clone().invert().multiply(selectedObject.matrix);
+                } else {
+                    selectedObject.matrix.copy(cursor.matrix.clone().multiply(initialGrabbed));
+                }
+            } else {
+                initialGrabbed = undefined;
+                selectedObject = undefined;
+            }
+
+        } else {
+            rayEnd.addVectors(cursor.position, direction.multiplyScalar(20));
+            lineFunc(1, rayEnd);
+        }
+
         renderer.render(scene, camera);
     }
     renderer.setAnimationLoop(render);
